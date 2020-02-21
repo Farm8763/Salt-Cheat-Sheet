4809d7ff7271eaefb34f9524e64212a55341ff4a
Ryan McNeely <rmcneely@teksavvy.com>
2020-02-20 22:41:27 -0500

    Create logging.py
    WIP

---
+++ _returners/logging.py
@@ -0,0 +1,226 @@
+# -*- coding: utf-8 -*-
+'''
+Return data to a remote syslog facility
+To use the syslog returner, append '--return logging' to the
+salt command.
+.. code-block:: bash
+    salt '*' test.ping --return syslog
+The following fields can be set in the minion conf file::
+    syslog.level (optional, Default: LOG_INFO)
+    syslog.facility (optional, Default: LOG_USER)
+    syslog.tag (optional, Default: salt-minion)
+    syslog.options (list, optional, Default: [])
+Available levels, facilities, and options can be found in the
+``syslog`` docs for your python version.
+.. note::
+    The default tag comes from ``sys.argv[0]`` which is
+    usually "salt-minion" but could be different based on
+    the specific environment.
+Configuration example:
+.. code-block:: yaml
+    syslog.level: 'LOG_ERR'
+    syslog.facility: 'LOG_DAEMON'
+    syslog.tag: 'mysalt'
+    syslog.options:
+      - LOG_PID
+Of course you can also nest the options:
+.. code-block:: yaml
+    syslog:
+      level: 'LOG_ERR'
+      facility: 'LOG_DAEMON'
+      tag: 'mysalt'
+      options:
+        - LOG_PID
+Alternative configuration values can be used by
+prefacing the configuration. Any values not found
+in the alternative configuration will be pulled from
+the default location:
+.. code-block:: yaml
+    alternative.syslog.level: 'LOG_WARN'
+    alternative.syslog.facility: 'LOG_NEWS'
+To use the alternative configuration, append
+``--return_config alternative`` to the salt command.
+.. versionadded:: 2015.5.0
+.. code-block:: bash
+    salt '*' test.ping --return syslog --return_config alternative
+To override individual configuration items, append
+--return_kwargs '{"key:": "value"}' to the salt command.
+.. versionadded:: 2016.3.0
+.. code-block:: bash
+    salt '*' test.ping --return syslog --return_kwargs '{"level": "LOG_DEBUG"}'
+.. note::
+    Syslog server implementations may have limits on the maximum
+    record size received by the client. This may lead to job
+    return data being truncated in the syslog server's logs. For
+    example, for rsyslog on RHEL-based systems, the default
+    maximum record size is approximately 2KB (which return data
+    can easily exceed). This is configurable in rsyslog.conf via
+    the $MaxMessageSize config parameter. Please consult your syslog
+    implmentation's documentation to determine how to adjust this limit.
+'''
+from __future__ import absolute_import, print_function, unicode_literals
+
+# Import python libs
+try:
+    import logging
+    import logging.handlers
+    HAS_LOGGING = True
+except ImportError:
+    HAS_LOGGING = False
+
+# Import Salt libs
+import salt.utils.jid
+import salt.utils.json
+import salt.returners
+from salt.ext import six
+
+log = logging.getLogger(__name__)
+# Define the module's virtual name
+__virtualname__ = 'logging'
+
+
+def _get_options(ret=None):
+    '''
+    Get the returner options from salt.
+    '''
+
+    defaults = {'level': 'LOG_INFO',
+                'facility': 'LOG_USER',
+                'options': []
+                }
+
+    attrs = {'level': 'level',
+             'facility': 'facility',
+             'tag': 'tag',
+             'options': 'options'
+             }
+
+    _options = salt.returners.get_returner_options(__virtualname__,
+                                                   ret,
+                                                   attrs,
+                                                   __salt__=__salt__,
+                                                   __opts__=__opts__,
+                                                   defaults=defaults)
+    return _options
+
+
+def _verify_options(options):
+    '''
+    Verify options and log warnings
+    Returns True if all options can be verified,
+    otherwise False
+    '''
+
+    # sanity check all vals used for bitwise operations later
+    bitwise_args = [('level', options['level']),
+                    ('facility', options['facility'])
+                    ]
+    bitwise_args.extend([('option', x) for x in options['options']])
+
+    for opt_name, opt in bitwise_args:
+        if not hasattr(logging, opt):
+            log.error('logging has no attribute %s', opt)
+            return False
+        if not isinstance(getattr(logging, opt), int):
+            log.error('%s is not a valid syslog %s', opt, opt_name)
+            return False
+
+    # Sanity check tag
+    if 'tag' in options:
+        if not isinstance(options['tag'], six.string_types):
+            log.error('tag must be a string')
+            return False
+        if len(options['tag']) > 32:
+            log.error('tag size is limited to 32 characters')
+            return False
+
+    return True
+
+
+def __virtual__():
+    if not HAS_LOGGING:
+        return False, 'Could not import logging returner; logging is not installed.'
+    return __virtualname__
+
+
+def returner(ret):
+    '''
+    Return data to the remote syslog
+    '''
+
+#    _options = _get_options(ret)
+
+#    if not _verify_options(_options):
+#        return
+
+    # Get values from syslog module
+#    level = getattr(syslog, _options['level'])
+#    facility = getattr(syslog, _options['facility'])
+
+    # parse for syslog options
+#    logoption = 0
+#    for opt in _options['options']:
+#        logoption = logoption | getattr(syslog, opt)
+
+    # Open syslog correctly based on options and tag
+    #if 'tag' in _options:
+    #    syslog.openlog(ident=salt.utils.stringutils.to_str(_options['tag']), logoption=logoption)
+    #else:
+    #    syslog.openlog(logoption=logoption)
+
+    # Send log of given level and facility
+    #syslog.syslog(facility | level, salt.utils.json.dumps(ret))
+
+    # Close up to reset syslog to defaults
+    #syslog.closelog()
+    my_logger = logging.getLogger('Salt-Master')
+    my_logger.setLevel(logging.DEBUG)
+
+    handler = logging.handlers.SysLogHandler(address = ('10.137.80.209', 514))
+
+    my_logger.addHandler(handler)
+
+    my_logger.critical(salt.utils.json.dumps(ret))
+    del my_logger
+
+def prep_jid(nocache=False,
+             passed_jid=None):  # pylint: disable=unused-argument
+    '''
+    Do any work necessary to prepare a JID, including sending a custom id
+    '''
+    return passed_jid if passed_jid is not None else salt.utils.jid.gen_jid(__opts__)
+
+def save_load(jid, load, minions=None):
+    '''
+    Return data to the local syslog
+    '''
+
+    #_options = _get_options()
+
+    #if not _verify_options(_options):
+    #    return
+
+    # Get values from syslog module
+    #level = getattr(syslog, _options['level'])
+    #facility = getattr(syslog, _options['facility'])
+
+    # parse for syslog options
+    #logoption = 0
+    #for opt in _options['options']:
+    #    logoption = logoption | getattr(syslog, opt)
+
+    # Open syslog correctly based on options and tag
+    #if 'tag' in _options:
+    #    syslog.openlog(ident=salt.utils.stringutils.to_str(_options['tag']), logoption=logoption)
+    #else:
+    #    syslog.openlog(logoption=logoption)
+
+    # Send log of given level and facility
+    #syslog.syslog(facility | level, salt.utils.json.dumps({}))
+
+    # Close up to reset syslog to defaults
+    #syslog.closelog()
+
+def get_load(jid):
+  ret = { "local": { "master_minion": { "fun_args": [], "jid": "20150330121011408195", "return":"2018.3.4","retcode": 0,"success": "true","cmd": "_return","_stamp":"2015-03-30T12:10:12.708663", "fun": "test.version", "id":"master_minion"  }}}
+  return ret
